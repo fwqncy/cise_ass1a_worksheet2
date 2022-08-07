3de422a1025b0afde08373c40e51c2098a11763f
Sayef <stickysayef@gmail.com>
2022-08-07 23:43:10 +1200

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const articles = require("./dummydata/articles");
 
 const server = express();
 
-server.listen(5000, console.log("server is working and listening PORT 5000"));
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
